d1f8d46f64c9990445743b86f1684c5ca66dd6e0
Daffabot <>
2023-04-13 18:29:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const reactViews = require('express-react-views');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', reactViews.createEngine());
+
+app.get('/', (req, res) => {
+	res.render('index.html');
+})
+
+app.listen(() => console.log(`server is up!`));
